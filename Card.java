0816c506249240df798cab5f120f1a54262bd279
DioneCheung <chee-tung-dione_cheung25@milton.edu>
2023-06-02 12:03:06 -0400

    Card class filled in

--- Card.java
+++ Card.java
@@ -16,6 +16,48 @@ public class Card {
         if(pSuit == 3){
             suit = "Spade";
         }
-        
+        if(pName == 0){
+            value = 1;
+        }
+        if(pName == 1){
+            value = 2;
+        }
+        if(pName == 2){
+            value = 3;
+        }
+        if(pName == 3){
+            value = 4;
+        }
+        if(pName == 4){
+            value = 5;
+        }
+        if(pName == 5){
+            value = 6;
+        }
+        if(pName == 6){
+            value = 7;
+        }
+        if(pName == 7){
+            value = 8;
+        }
+        if(pName == 8){
+            value = 9;
+        }
+        if(pName == 9){
+            value = 10;
+        }
+        if(pName == 10){
+            value = 10;
+        }
+        if(pName == 11){
+            value = 10;
+        }
+        if(pName == 12){
+            value = 10;
+        }
+        if(pName == 13){
+            value = 10;
+        }
+
     }
 }
